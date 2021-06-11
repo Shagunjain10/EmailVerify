94444fde52c7bcede7a67f396f05c029d9e79612
ShagunJain10 <jshagun710@gmail.com>
2021-06-11 16:40:48 +0530

    update settings

--- EmailVerify/settings.py
+++ EmailVerify/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from decouple import config
+# from decouple import config
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('sendingMail')
+EMAIL_HOST_USER = 'jshagun7100@gmail.com'
 EMAIL_HOST_PASSWORD = 'Shagun#321'
